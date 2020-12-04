e3499e2cd07a509d54ec69a273e0091339f03d0b
lapis-lazulon <matthew.gilmore18@gmail.com>
2020-12-04 12:53:53 +0000

    Day 3 Complete

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -11,4 +11,15 @@ for y in range(0, 323):
     x = (x + 3)%31
 print(count)
 
+#In[]
+def tree_detection(down,right):
+    counter = 0
+    x = 0
+    for y in range(0,323,down):
+        if tree_map[0][y][x] == '#':
+            counter = counter + 1
+        x = (x + right)%31
+    return counter
+
+tree_detection(1,3)*tree_detection(1,1)*tree_detection(1,5)*tree_detection(1,7)*tree_detection(2,1)
 # %%
