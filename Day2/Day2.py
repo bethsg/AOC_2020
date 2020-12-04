38893551cd2cfc80c781cb89683928eb4214f7d2
lapis-lazulon <matthew.gilmore18@gmail.com>
2020-12-04 12:14:37 +0000

    Day 2 Complete

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -12,5 +12,13 @@ for n in range(0, len(passwords)):
     if low <= temp <= high:
         count = count + 1
 print(count)
-# %%
+# In[]
+count = 0
+for n in range(0, len(passwords)):
+    pos1, pos2 = passwords[n,0].split("-")
+    pos1 = int(pos1)
+    pos2 = int(pos2)
+    if (passwords[n,1][0] == passwords[n,2][pos1-1]) ^ (passwords[n,1][0] == passwords[n,2][pos2-1]):
+        count = count + 1
+print(count)
 # %%
