a5d45b77f803954d5508099711ef86695983046f
lapis-lazulon <matthew.gilmore18@gmail.com>
2020-12-07 18:08:06 +0000

    Day 6 Complete

--- Day6/Day6.py
+++ Day6/Day6.py
@@ -2,9 +2,10 @@
 with open("Input6.txt") as f:
     inputt  = f.read()
 groups  = inputt.split("\n\n")
+groups[-1] = groups[-1][:-1]
 
-alphabet = set("abcdefghijklmnopgrstuvwxyz")
-
+alphabet = set("abcdefghijklmnopqrstuvwxyz")
+#In[]
 sum_of_answers = 0
 for group in groups:
     group_answers = 0
@@ -15,4 +16,14 @@ for group in groups:
 
 print(sum_of_answers)
     
+#In[]
+sum_of_counts = 0
+for group in groups:
+    group_size = len(group.split('\n'))
+    for char in alphabet:
+        if group_size == group.count(char):
+            sum_of_counts = sum_of_counts + 1
+print(sum_of_counts)
+
+
 # %%
